3a938501f4e42058a914d4261f25c105a8e2b4cc
Gabriel Lira <gabrielfllira@gmail.com>
2024-04-06 14:53:26 -0300

    Commit Inicial

---
+++ calc.print.js
@@ -0,0 +1,17 @@
+import {calc} from ".calc.js";
+
+//add
+console.log(calc(1, 1, "+"));
+console.log(2);
+
+//sub
+console.log(calc(1, 1, "-"));
+console.log(0);
+
+//mult
+console.log(calc(1, 1, "*"));
+console.log(1);
+
+//div
+console.log(calc(1, 1, "/"));
+console.log(1);
